760d3426b6bce54ab18ecb392809c42598726fd9
manish <manishk@infibeam.net>
2013-06-08 18:32:39 +0530

    requirement specifications

---
+++ spec/plateau_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'Plateau' do
+  it 'should raise invalid coordinates error on initialize if the same are not valid'
+  it 'should say true for asking a valid coordinates presence in it'
+  it 'should say false for asking a coordinates outside of it'
+end
---
+++ spec/rover_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'Rover' do
+  it 'should tell its position right'
+  it 'should move to right on "R"'
+  it 'should move to left on "L"'
+  it 'should go ahead on "M"'
+end
