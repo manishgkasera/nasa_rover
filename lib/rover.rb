3bc14931e6af636f3ddf7f4fc731f3ec2b001251
manish <manishk@infibeam.net>
2013-06-09 14:11:19 +0530

    Rover added

---
+++ lib/rover.rb
@@ -0,0 +1,71 @@
+require 'plateau'
+require 'compass_point'
+
+#Rover represents a rover on a plateau,
+#it has a position which are valid coordinates on a plateau plus a compass direction
+class Rover
+  attr_reader :plateau, :pos_x, :pos_y, :compass_point
+
+  def initialize(plateau, pos_x=0, pos_y=0, face=:N)
+    if plateau.have_coordinate?(pos_x, pos_y)
+      @plateau, @pos_x, @pos_y, @compass_point = plateau, pos_x, pos_y, CompassPoint.new(face)
+    else
+      raise Invalid
+    end
+  end
+
+
+  def move(directions)
+    old_position = self.position
+    directions.each_char do |direction|
+      case direction
+      when 'L'
+        compass_point.move_to_left
+      when 'R'
+        compass_point.move_to_right
+      when 'M'
+        begin
+          case compass_point.face
+          when :N
+            move_to_coordinate(@pos_x, @pos_y+1)
+          when :E
+            move_to_coordinate(@pos_x+1, @pos_y)
+          when :S
+            move_to_coordinate(@pos_x, @pos_y-1)
+          when :W
+            move_to_coordinate(@pos_x-1, @pos_y)
+          end
+        rescue CannotMove
+          restore_position(old_position)
+          raise
+        end
+      else
+        raise "Invalid Move command #{direction}, should be one of L,R,M"
+      end
+    end
+  end
+
+  def position
+    "#{pos_x} #{pos_y} #{compass_point.face}"
+  end
+  alias inspect position
+  alias to_s inspect
+
+  class CannotMove < StandardError; end
+
+  private
+    def move_to_coordinate(x, y)
+      if plateau.have_coordinate?(x, y)
+        @pos_x, @pos_y = x, y
+      else
+        raise CannotMove
+      end
+    end
+
+    def restore_position(old_position)
+      pos = old_position.split(' ')
+      @compass_point = CompassPoint.new(pos.pop.to_sym)
+      @pos_x, @pos_y = pos.map(&:to_i)
+    end
+
+end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,8 +1,63 @@
 require 'spec_helper'
+require 'plateau'
+require 'rover'
 
 describe 'Rover' do
-  it 'should tell its position right'
-  it 'should move to right on "R"'
-  it 'should move to left on "L"'
-  it 'should go ahead on "M"'
+  it 'should tell its position right' do
+    p = Plateau.new(0,0,10,10)
+    r = Rover.new(p, 5,5,:N)
+    r.position.should eq('5 5 N')
+  end
+
+  it 'should move to right on "R"' do
+    p = Plateau.new(0,0,10,10)
+    r = Rover.new(p, 5,5,:N)
+    r.move('R')
+    r.position.should eq('5 5 E')
+    r.move('R')
+    r.position.should eq('5 5 S')
+    r.move('R')
+    r.position.should eq('5 5 W')
+    r.move('R')
+    r.position.should eq('5 5 N')
+  end
+
+  it 'should move to left on "L"' do
+    p = Plateau.new(0,0,10,10)
+    r = Rover.new(p, 5,5,:N)
+    r.move('L')
+    r.position.should eq('5 5 W')
+    r.move('L')
+    r.position.should eq('5 5 S')
+    r.move('L')
+    r.position.should eq('5 5 E')
+    r.move('L')
+    r.position.should eq('5 5 N')
+  end
+
+  it 'should go ahead on "M"' do
+    p = Plateau.new(0,0,10,10)
+    r = Rover.new(p, 5,5,:N)
+    r.move('M')
+    r.position.should eq('5 6 N')
+    r.move('MR')
+    r.position.should eq('5 7 E')
+    r.move('MR')
+    r.position.should eq('6 7 S')
+    r.move('ML')
+    r.position.should eq('6 6 E')
+  end
+
+  it 'should raise exception when moving to invalid coordinate' do
+    p = Plateau.new(0,0,5,5)
+    r = Rover.new(p, 5,5,:N)
+
+    ['M', 'MR', 'RM'].each do |command|
+      begin
+        r.move(command)
+        1.should eq(2), "Rover should not be moved on plateau #{p} with position #{r.position} on #{command} command"
+      rescue Rover::CannotMove
+      end
+    end
+  end
 end
